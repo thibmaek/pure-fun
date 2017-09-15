5be871bfa92a3ccd7db88582c41e28afa4dc536c
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-09-15 13:01:58 +0200

    :white_check_mark: Added unit test for flatten()

---
+++ arrays/__tests__/flatten.test.js
@@ -0,0 +1,9 @@
+const flatten = require('../flatten');
+
+const multiDimensionalArray = [1, [2, [3]]];
+
+describe('flatten()', () => {
+  it('flattens a multidimensional array', () => {
+    expect(flatten(multiDimensionalArray)).toEqual([1, 2, 3]);
+  });
+});
--- arrays/flatten.js
+++ arrays/flatten.js
@@ -3,5 +3,7 @@
  * @param {Array<Any>} arr [A collection of other types]
  * @return {Array<Any>} - Single dimension collection
 **/
-module.exports = arr =>
+const flatten = arr =>
   arr.reduce((a, b) => a.concat(Array.isArray(b) ? flatten(b) : b), []);
+
+module.exports = flatten;
