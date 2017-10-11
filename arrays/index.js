aa7929162151bcb45837f98e9ae430e82620fb32
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2017-10-11 13:07:35 +0200

    🚚 Add export for arrays/zip

--- arrays/index.js
+++ arrays/index.js
@@ -2,10 +2,12 @@ import filterFalsies from './filterFalsies';
 import flatten from './flatten';
 import pickRandom from './pickRandom';
 import randomize from './randomize';
+import zip from './zip';
 
 export default {
   filterFalsies,
   flatten,
   pickRandom,
   randomize,
+  zip,
 };
