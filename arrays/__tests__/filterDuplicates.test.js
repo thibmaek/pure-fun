835207d991ccfe01624cb098fdf46ca97afbb242
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-10-12 14:25:21 +0200

    💚 Fix build #8
    https://travis-ci.org/thibmaek/pure-fun/builds/287011775
    - Changed boxed string to not construct a new instance.

--- arrays/__tests__/filterDuplicates.test.js
+++ arrays/__tests__/filterDuplicates.test.js
@@ -1,6 +1,6 @@
 import filterDuplicates from '../filterDuplicates';
 
-const arrayWithDupes = [1, 2, 1, `a`, `b`, new String(`a`)];
+const arrayWithDupes = [1, 2, 1, `a`, `b`, String(`a`)];
 
 describe(`[Arrays]: filterDuplicates`, () => {
   it(`should return an array without duplicate values`, () => {
