cea551fb1c27a8a102b97276f9d1b9c96f3d4fd5
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-10-23 12:45:16 +0200

    ⚡️ Add objects/makeImmutable

---
+++ objects/__tests__/makeImmutable.test.js
@@ -0,0 +1,17 @@
+import immutable from '../makeImmutable';
+
+const obj = { a: 1, b: 2, c: 3 };
+const immutableObj = immutable(obj);
+
+describe(`[Objects]: makeImmutable`, () => {
+  it(`should stop properties from being modified or created`, () => {
+    expect(() => immutableObj.a = 3).toThrowError(TypeError);
+    expect(() => Object.assign(immutableObj, { d: 4 })).toThrowError(TypeError);
+    expect(() => Object.defineProperty(immutableObj, `d`, 4)).toThrowError(TypeError);
+  });
+
+  it(`should be frozen & sealed`, () => {
+    expect(Object.isFrozen(immutableObj)).toEqual(true);
+    expect(Object.isSealed(immutableObj)).toEqual(true);
+  });
+});
