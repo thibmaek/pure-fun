99705cbbf47f7db460092581dddc9d6df0f30a48
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-10-23 13:01:27 +0200

    ✨ Add arrays/total

---
+++ arrays/total.js
@@ -0,0 +1,6 @@
+/**
+ * Get the total sum for an array of numbers
+ * @param  {Array<Number>} arr an array containing number values
+ * @return {Number}
+ */
+export default arr => arr.reduce((a, b) => a + b);
