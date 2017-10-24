1db00ce882a51bc17a7ce35fd19f9d1132c7cbeb
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-10-24 13:19:02 +0200

    ✨ Added arrays/head

---
+++ arrays/head.js
@@ -0,0 +1,6 @@
+/**
+ * Returns the first value in an array
+ * @param  {Array<Any>} arr an array containing any type of value
+ * @return {Any} the first value in the array
+ */
+export default arr => arr[0];
