c41965d81902245f0442aa87e7294bdf22c9d0a1
Thibault Maekelbergh <thibault.maekelbergh@iCloud.com>
2017-10-24 13:19:31 +0200

    ✨ Added arrays/last

---
+++ arrays/last.js
@@ -0,0 +1,6 @@
+/**
+ * Rerturn the last item in an array
+ * @param  {Array<Any>} arr an array containing any type of value
+ * @return {Any} the last value in the array
+ */
+export default arr => arr.slice(- 1)[0];
