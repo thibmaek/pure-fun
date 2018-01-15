f3f4fb1f65054dd23406c47ccc2019983571c283
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2018-01-15 22:56:53 +0100

    Move tests out of src folder
    flow-remove-types would copy these over, creating duplicates

--- src/objects/__tests__/pluckDeep.test.js
+++
@@ -1,9 +0,0 @@
-import pluckDeep from '../pluckDeep';
-
-const deepObj = { a: { b: { c: `✓` } } };
-
-describe(`[Objects]: pluckDeep`, () => {
-  it(`should return the value for a given key`, () => {
-    expect(pluckDeep(`a.b.c`)(deepObj)).toEqual(`✓`);
-  });
-});
