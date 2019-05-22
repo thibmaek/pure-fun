49fd616c5aa2de9220b34a7e6d7561ed85f0b4c3
Thibault Maekelbergh <thibault.maekelbergh@icloud.com>
2019-05-22 21:32:31 +0200

    getDays, getMonths: Fix locale issue

--- src/dates/getDays/getDays.test.js
+++ src/dates/getDays/getDays.test.js
@@ -2,14 +2,14 @@ import getDays from '.';
 
 describe(`[Dates]: getDays`, () => {
   it(`without any options should return US American long formatted dates`, () => {
-    expect(getDays()).toEqual([`Saturday`, `Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`]);
+    expect(getDays()).toEqual([`Sunday`, `Monday`, `Tuesday`, `Wednesday`, `Thursday`, `Friday`, `Saturday`]);
   });
 
   it(`with the nl-BE locale`, () => {
-    expect(getDays(`nl-BE`)).toEqual([`zaterdag`, `zondag`, `maandag`, `dinsdag`, `woensdag`, `donderdag`, `vrijdag`]);
+    expect(getDays(`nl-BE`)).toEqual([`zondag`, `maandag`, `dinsdag`, `woensdag`, `donderdag`, `vrijdag`, `zaterdag`]);
   });
 
   it(`with a short format`, () => {
-    expect(getDays(`en-US`, `short`)).toEqual([`Sat`, `Sun`, `Mon`, `Tue`, `Wed`, `Thu`, `Fri`]);
+    expect(getDays(`en-US`, `short`)).toEqual([`Sun`, `Mon`, `Tue`, `Wed`, `Thu`, `Fri`, `Sat`]);
   });
 });
--- src/dates/getMonths/getMonths.test.js
+++ src/dates/getMonths/getMonths.test.js
@@ -2,14 +2,17 @@ import getMonths from '../getMonths';
 
 describe(`[Dates]: getMonths`, () => {
   it(`without any options should return US American long formatted dates`, () => {
-    expect(getMonths()).toEqual([`December`, `January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`]);
+    expect(getMonths())
+      .toEqual([`January`, `February`, `March`, `April`, `May`, `June`, `July`, `August`, `September`, `October`, `November`, `December`]);
   });
 
   it(`with the nl-BE locale`, () => {
-    expect(getMonths(`nl-BE`)).toEqual([`december`, `januari`, `februari`, `maart`, `april`, `mei`, `juni`, `juli`, `augustus`, `september`, `oktober`, `november`]);
+    expect(getMonths(`nl-BE`))
+      .toEqual([`januari`, `februari`, `maart`, `april`, `mei`, `juni`, `juli`, `augustus`, `september`, `oktober`, `november`, `december`]);
   });
 
   it(`with a short format`, () => {
-    expect(getMonths(`en-US`, `short`)).toEqual([`Dec`, `Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov`]);
+    expect(getMonths(`en-US`, `short`))
+      .toEqual([`Jan`, `Feb`, `Mar`, `Apr`, `May`, `Jun`, `Jul`, `Aug`, `Sep`, `Oct`, `Nov`, `Dec`]);
   });
 });
